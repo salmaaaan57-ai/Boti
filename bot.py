4693401fe36b468eafdd4e0da4145fb671018d8d
salmaaaan57-ai <salmaaaan57@gmail.com>
2026-06-27 18:49:46 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -18,7 +18,7 @@ TOKEN = "8787127714:AAEOsu05CBrLstCCs0OQWVMoA3f5TuVQ5CI" # توكن البوت
 ADMIN_ID = 7556662373         # رقمك التعريفي
 CHANNEL_ID = "-1003869521696" # معرف القناة
 WEBAPP_URL = "https://salmaaaaan57-ai.github.io/mithaq-form/"
-" # Github Pages لاحقاً
+ # Github Pages لاحقاً
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher(storage=MemoryStorage())
